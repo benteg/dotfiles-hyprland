8c27b72992abddd37ee07172c7e2e16345c0d902
Bente Groh <mail@bentegroh.de>
2023-05-11 19:36:56 +0200

    bat

--- waybar/scripts/light.py
+++ waybar/scripts/light.py
@@ -5,13 +5,13 @@ min_val = 15
 
 with open("/etc/light/targets/sysfs/backlight/intel_backlight/save", "r") as f:
     raw_val = int(f.readline())
-    restr_val = round(raw_val/8.92)
+    restr_val = round(raw_val / 8.92)
 
 if cur_val > 25:
     subprocess.run(["light", "-O"])
     subprocess.run(["light", "-S", str(min_val)])
 elif cur_val <= 25:
-        if restr_val >= cur_val+10:
-            subprocess.run(["light", "-I"])
+    if restr_val >= cur_val + 10:
+        subprocess.run(["light", "-I"])
 
-        subprocess.run(["light", "-S", "100"])
+    subprocess.run(["light", "-S", "100"])
